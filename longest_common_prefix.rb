9b71d8801f531c3672d3e2a112e778129129b55c
Dylan Connolly <dylanconnolly145@gmail.com>
2020-08-12 16:53:10 -0600

    Working loop, time compile issue

--- longest_common_prefix.rb
+++ longest_common_prefix.rb
@@ -4,7 +4,13 @@ def longest_common_prefix(strs)
   # split array into each word
   common = []
   # take first letter of first word
-  strs[0]
+
+  index = strs[0].first
+
+  strs.each do |word|
+    if word.split('').shift == index
+    end
+  end
   # compare against first letter of other words
   # if match, move to second letter
   # as soon as there isn't a match, break and return what matched
