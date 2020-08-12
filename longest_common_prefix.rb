4d82768bd98cf8eef2d4fc813c4422291f6c75ac
Dylan Connolly <dylanconnolly145@gmail.com>
2020-08-12 16:48:47 -0600

    Add pseudo code for problem

--- longest_common_prefix.rb
+++ longest_common_prefix.rb
@@ -1,5 +1,17 @@
 require 'pry'
 
 def longest_common_prefix(strs)
-  
+  # split array into each word
+  common = []
+  # take first letter of first word
+  strs[0]
+  # compare against first letter of other words
+  # if match, move to second letter
+  # as soon as there isn't a match, break and return what matched
 end
+
+strings = ["flower","flow","flight"]
+
+longest_common_prefix(strings)
+
+binding.pry
